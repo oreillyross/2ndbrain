a56acf6ad7bbc9e8538807a13293fb87d716931f
Faktor 10 <oreillyross@gmail.com>
2026-03-16 10:59:00 +0000

    start again

--- server/routers/health.ts
+++
@@ -1,7 +0,0 @@
-import { publicProcedure, router } from "../trpc/trpc";
-
-export const healthRouter = router({
-  ping: publicProcedure.query(() => {
-    return { status: "ok" };
-  }),
-});
--- server/trpc/context.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "../db";
-
-export const createContext = () => {
-  return { db };
-};
-
-export type Context = Awaited<ReturnType<typeof createContext>>;
